63989e266bd712525683e6cef22f18390d37ce80
Wallace ferreira <wallace.programador2024@gmail.com>
2025-03-04 21:55:54 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogo de numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = ' Escolha um número entre 1 e 10';
